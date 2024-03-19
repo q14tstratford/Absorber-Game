00f53e1eb6053afa0f5f50c8d90d3c1ffce217e0
q14tstratford <163541045+q14tstratford@users.noreply.github.com>
2024-03-19 12:55:58 +0000

    Add files via upload

--- Absorbin'_Time.py
+++ Absorbin'_Time.py
@@ -1,6 +1,7 @@
 #Imports
 
 from pygame_functions import *
+import math
 
 #Setup
 
@@ -20,13 +21,46 @@ class Creature:
         self.x=x
         self.y=y
         self.sprite=makeSprite(image)
-        moveSprite(self.sprite,self.x,self.y)
+        self.direction=0
+        self.size=size
+        self.speed = 10
+        moveSprite(self.sprite,self.x,self.y, centre=True)
         showSprite(self.sprite)
+        transformSprite(self.sprite,self.direction,self.size)
+        
+    def move(self):
+        mX=mouseX()
+        mY=mouseY()
+        wP=keyPressed("W")
+        aP=keyPressed("A")
+        sP=keyPressed("S")
+        dP=keyPressed("D")
+        dX=mX-self.x
+        dY=mY-self.y
+        self.direction=math.atan2(dY,dX)
+        transformSprite(self.sprite,self.direction/math.pi*180+90,self.size)
+        xspeed = 0
+        yspeed = 0
+        if wP:
+            xspeed = self.speed * math.cos(self.direction)
+            yspeed = self.speed * math.sin(self.direction)
+        elif sP:
+            xspeed = -self.speed * math.cos(self.direction)
+            yspeed = -self.speed * math.sin(self.direction)
+        elif aP:
+            xspeed = -0.75*self.speed * math.cos(self.direction+math.pi/2)
+            yspeed = -0.75*self.speed * math.sin(self.direction+math.pi/2)
+        self.x += xspeed
+        self.y += yspeed
+        moveSprite(self.sprite, self.x, self.y, centre=True)
 #Creatures
 
-c1=Creature(0,0,1,"SweetBabyKevin.png")
+Player=Creature(150,150,0.5,"Kevin.png")
 
+#e1=Creature(200,200,0.5,"Bob.png",0)
 #Main Body
-
+while True:
+    Player.move()
+    tick(30)
 
 endWait()
