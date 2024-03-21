0a7f0232bfbab75a7b54f4e908b4383fc8158e3f
q14tstratford <163541045+q14tstratford@users.noreply.github.com>
2024-03-21 13:00:04 +0000

    Add files via upload

--- Absorbin'_Time.py
+++ Absorbin'_Time.py
@@ -2,10 +2,10 @@
 
 from pygame_functions import *
 import math
-
+import random
 #Setup
 
-screenSize(500,500)
+screenSize(1000,800)
 setBackgroundColour("blue")
 
 #Globals
@@ -41,15 +41,43 @@ class Creature:
         transformSprite(self.sprite,self.direction/math.pi*180+90,self.size)
         xspeed = 0
         yspeed = 0
-        if wP:
+        speedScalar=((dX**2+dY**2)**0.5)/100
+        if speedScalar>1.25:
+            speedScalar==1.25
+        if abs(dX)>5 or abs(dY)>5:
+            if wP:
+                xspeed = self.speed * math.cos(self.direction)*speedScalar
+                yspeed = self.speed * math.sin(self.direction)*speedScalar
+            elif sP:
+                xspeed = -self.speed * math.cos(self.direction)*speedScalar
+                yspeed = -self.speed * math.sin(self.direction)*speedScalar
+            elif aP:
+                xspeed = -1.05*self.speed * math.cos(self.direction+math.pi/2)
+                yspeed = -1.05*self.speed * math.sin(self.direction+math.pi/2)
+            elif dP:
+                xspeed = 1.05*self.speed * math.cos(self.direction+math.pi/2)
+                yspeed = 1.05*self.speed * math.sin(self.direction+math.pi/2)
+        self.x += xspeed
+        self.y += yspeed
+        moveSprite(self.sprite, self.x, self.y, centre=True)
+
+class Enemy(Creature):
+    def move(self):
+        global Player
+        dX=Player.x-self.x
+        dY=Player.y-self.y
+        xspeed=0
+        yspeed=0
+        if ((dX**2+dY**2)**0.5)<350 and self.size>Player.size:
+            self.direction=math.atan2(dY,dX)
+            transformSprite(self.sprite,self.direction/math.pi*180+90,self.size)
             xspeed = self.speed * math.cos(self.direction)
             yspeed = self.speed * math.sin(self.direction)
-        elif sP:
-            xspeed = -self.speed * math.cos(self.direction)
-            yspeed = -self.speed * math.sin(self.direction)
-        elif aP:
-            xspeed = -0.75*self.speed * math.cos(self.direction+math.pi/2)
-            yspeed = -0.75*self.speed * math.sin(self.direction+math.pi/2)
+        elif ((dX**2+dY**2)**0.5)<350 and self.size<=Player.size:
+            self.direction=math.atan2(-dY,-dX)
+            transformSprite(self.sprite,self.direction/math.pi*180+90,self.size)
+            xspeed = self.speed * math.cos(self.direction)
+            yspeed = self.speed * math.sin(self.direction)
         self.x += xspeed
         self.y += yspeed
         moveSprite(self.sprite, self.x, self.y, centre=True)
@@ -57,10 +85,14 @@ class Creature:
 
 Player=Creature(150,150,0.5,"Kevin.png")
 
+enemies=[]
+for i in range(5):
+    enemies.append(Enemy(random.randint(0,1000),random.randint(0,800),random.randint(0,10)/10,"Bob.png"))
 #e1=Creature(200,200,0.5,"Bob.png",0)
 #Main Body
 while True:
     Player.move()
     tick(30)
-
+    for e in enemies:
+        e.move()
 endWait()
